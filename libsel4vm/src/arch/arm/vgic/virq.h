d1c6caba7906f7c3b21962a0b745e53e3a608edf
Axel Heider <axel.heider@hensoldt.net>
2022-07-14 23:50:46 +0200

    libsel4vm: inline code into virq_find_irq_data()
    All functions are only use once, inlining them simplifies the code.
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- libsel4vm/src/arch/arm/vgic/virq.h
+++ libsel4vm/src/arch/arm/vgic/virq.h
@@ -88,30 +88,30 @@ static inline vgic_vcpu_t *get_vgic_vcpu(vgic_t *vgic, int vcpu_id)
     return &(vgic->vgic_vcpu[vcpu_id]);
 }
 
-static inline struct virq_handle *virq_get_sgi_ppi(vgic_t *vgic, vm_vcpu_t *vcpu, int virq)
+static inline struct virq_handle *virq_find_irq_data(struct vgic *vgic, vm_vcpu_t *vcpu, int virq)
 {
-    vgic_vcpu_t *vgic_vcpu = get_vgic_vcpu(vgic, vcpu->vcpu_id);
-    assert(vgic_vcpu);
-    assert((virq >= 0) && (virq < ARRAY_SIZE(vgic_vcpu->local_virqs)));
-    return vgic_vcpu->local_virqs[virq];
-}
+    assert(vgic);
+    assert(vcpu);
+    assert(virq >= 0);
 
-static inline struct virq_handle *virq_find_spi_irq_data(struct vgic *vgic, int virq)
-{
+    if (virq < NUM_VCPU_LOCAL_VIRQS)  {
+        /* find vCPU local interrupt (SGI, PPI). */
+        vgic_vcpu_t *vgic_vcpu = get_vgic_vcpu(vgic, vcpu->vcpu_id);
+        assert(vgic_vcpu);
+        assert(virq < ARRAY_SIZE(vgic_vcpu->local_virqs));
+        return vgic_vcpu->local_virqs[virq];
+    }
+
+    /* Find global interrupt (SPI). */
     for (int i = 0; i < ARRAY_SIZE(vgic->vspis); i++) {
         if (vgic->vspis[i] && vgic->vspis[i]->virq == virq) {
             return vgic->vspis[i];
         }
     }
-    return NULL;
-}
 
-static inline struct virq_handle *virq_find_irq_data(struct vgic *vgic, vm_vcpu_t *vcpu, int virq)
-{
-    if (virq < NUM_VCPU_LOCAL_VIRQS)  {
-        return virq_get_sgi_ppi(vgic, vcpu, virq);
-    }
-    return virq_find_spi_irq_data(vgic, virq);
+    /* Interrupt not registered. */
+    return NULL;
+
 }
 
 static inline int virq_register(vm_vcpu_t *vcpu, vgic_t *vgic, int irq,
